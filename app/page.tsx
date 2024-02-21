48dc1dd0fb9eee7e85062d149a89a6abf319f859
HyeonSuJeon <91925915+jhs-peernine@users.noreply.github.com>
2024-02-21 22:53:38 +0900

    Feat: init

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+  const basePath = process.env.NODE_ENV === 'production' ? '/invitation-template' : '';
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -17,7 +18,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="./vercel.svg"
+              src={`${basePath}/vercel.svg`}
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -31,12 +32,15 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="./next.svg"
+          src={`${basePath}/next.svg`}
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
         />
+      </div>
+      <div className="relative flex place-items-center">
+        <span>with <b><span style={{ color: 'green' }}>NH</span> <span style={{ color: 'blue' }}>Choi</span></b></span>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
